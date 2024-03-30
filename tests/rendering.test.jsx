4d550bf5bc718963f3947ca3aa92994ef141dfce
Charles Seitz <charlesjseitz@gmail.com>
2024-03-29 22:05:13 -0400

    built develop

--- tests/rendering.test.jsx
+++ tests/rendering.test.jsx
@@ -5,6 +5,6 @@ import TwitterLogin from 'TwitterLogin';
 describe('TwitterLogin', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<TwitterLogin/>, div);
+    ReactDOM.render(<TwitterLogin loginUrl="https://localhost:8080/" requestTokenUrl="https://localhost:8080/reverse" onFailure={console.log("failed")} onSuccess={console.log("success")}/>, div);
   });
 });
